5f79613e403009b76fb28f23c34d3bb4b1b3913f
Alex Vilson <104514885+Alexvilson97@users.noreply.github.com>
2024-12-31 00:32:37 +0100

    SSIM

---
+++ Annotations/Rain_scenario/README.dataset.txt
@@ -0,0 +1,6 @@
+# Rain_scenario > 2024-12-30 12:07am
+https://universe.roboflow.com/labeling-ujkwf/rain_scenario
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
--- TPR_cal.py
+++ TPR_cal.py
@@ -109,8 +109,8 @@ def process_csv(csv_path, confidence_levels, iou_thresholds):
     return metrics_results
 
 # Define CSV files for real and synthetic data
-real_csv_files = [r"combined_csv\Night_Highway\RD_night_highway.csv"]
-synthetic_csv_files = [r"combined_csv\Night_Highway\SD_night_highway.csv"]
+real_csv_files = [r"combined_csv\Rainy_residential\RD_Rain_combined.csv"]
+synthetic_csv_files = [r"combined_csv\Rainy_residential\SD_Rain_combined.csv"]
 
 confidence_levels = np.arange(0.3, 1.0, 0.1)
 iou_thresholds = [0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
